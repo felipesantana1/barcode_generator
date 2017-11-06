1e6b3562f0ae303b4ac4868db68a7047ec7022f5
Felipe <felipeo510.fs@gmail.com>
2017-11-06 12:35:00 -0800

    Barcode assignment complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'barcode';
+  bar_color: Array<string> = [];
+  colors: Array<string> = ["AliceBlue","Chocolate","Aqua","BlueViolet","ForestGreen","Gold","LightBlue","Orchid","Purple"];
+
+  fill_barcode(){
+    
+    for(let i = 0; i < this.colors.length; i++){
+      let index: number = Math.floor(Math.random()*this.colors.length);
+      this.bar_color.push(this.colors[index]);
+    }
+  }
+
+  ngOnInit(){
+    this.fill_barcode();
+  }
+  
 }
